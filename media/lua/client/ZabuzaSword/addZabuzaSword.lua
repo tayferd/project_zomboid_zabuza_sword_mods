ce9ef189cb39dbaaf6d73039fa6bc08a03d88f65
Taylor Ferderer <110494569+tayferd@users.noreply.github.com>
2024-08-04 15:14:49 -0400

    Update addZabuzaSword.lua

---
+++ media/lua/client/ZabuzaSword/addZabuzaSword.lua
@@ -0,0 +1,6 @@
+local function initZabuzaSword()
+    -- Add any custom behavior or initialization here
+    print("Zabuza's sword has been added to the game.")
+end
+
+Events.OnGameStart.Add(initZabuzaSword)
